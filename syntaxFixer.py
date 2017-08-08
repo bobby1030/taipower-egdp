faee0f2ac62139e6df3f316eb16a511bde4c0228
BobbyHo <angela.bobby@gmail.com>
2017-08-08 23:07:24 +0800

    Add keyRenamer & tidy the code of datetimeFixer

--- syntaxFixer.py
+++ syntaxFixer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 def datetimeFixer(str):
-	findStr = '"":"'
-	replaceWithStr = '"datetime":"'
-	str = str.replace(findStr, replaceWithStr)
+	str = str.replace('"":"', '"datetime":"')
+	return(str)
+
+def keyRenamer(str):
+	str = str.replace('aaData', 'data')
 	return(str)
 
 def syntaxFixer(sourceData):
 	sourceData = datetimeFixer(sourceData)
+	sourceData = keyRenamer(sourceData)
 	return(sourceData)
